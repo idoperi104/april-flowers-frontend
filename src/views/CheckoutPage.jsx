2aef4d6ea59b0d8f14eb33d6303e3b6af231f0d2
idoperi104 <idoperi104@gmail.com>
2023-11-19 14:20:36 +0200

    commit

--- src/views/CheckoutPage.jsx
+++ src/views/CheckoutPage.jsx
@@ -39,6 +39,7 @@ export function CheckoutPage() {
   }
 
   async function onSaveOrder(ev) {
+    ev.preventDefault()
     dispatch(resetCartItems())
     try {
       await orderService.save({ ...order, isPaid: true, createdAt: Date.now() })
@@ -47,7 +48,6 @@ export function CheckoutPage() {
     } catch (error) {
       console.log("error:", error)
     }
-    ev.preventDefault()
   }
 
   function onUpdateCartItemQuantity(cartItem, num) {
